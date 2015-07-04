eed7049f25871bcb9095a163df083a0e8b4fcdde
azitowolf <azito.wolf3@gmail.com>
2015-07-04 13:36:27 -0400

    threw all bubblegrid functions into a constructor function that create BG instances

--- js/app.js
+++ js/app.js
@@ -1,138 +1,22 @@
-var fullwidth = $(window).width();
-var fullheight = $(window).height();
+  var fullwidth = $(window).width();
+  var fullheight = $(window).height();
 
-var createBubble = function(div, id, xOffset, yOffset, diameter) {
-  var html = '<div class="circle" id="C' + id + '"><div><h3>content</h3></div></div>'
-  div.append(html);
-  $('#C' + id).css('top', yOffset);
-  $('#C' + id).css('left', xOffset);
-  $('#C' + id).css('width', diameter).css('height', diameter);
-}
+  var startPos = {
+    x: fullwidth / 2,
+    y: fullheight / 2
+  };
 
-$('#submit').click(function() {
-  var numberOfRings = $('#numberOfRings').val();
-  var numberOfCircles = $('#numberOfCircles').val();
-  var circleRadii = $('#circleRadii').val();
-  var circleRatio = $('#circleRatio').val();
-  $('#work').html(' ');
-  bubbleGridPosition(circleRadii);
-})
+  $('#submit').click(function() {
+    var numberOfRings = $('#numberOfRings').val();
+    var numberOfCircles = $('#numberOfCircles').val();
+    var circleRadii = $('#circleRadii').val();
+    var circleRatio = $('#circleRatio').val();
+    $('#work').html(' ');
+    var bubbleGrid = {};
+    bubbleGrid = new BubbleGrid(circleRadii, 3);
+    bubbleGrid.bubbleGridStart();
+    bubbleGrid.findThirdPoint(bubbleGrid.bubbleGrid[0], bubbleGrid.bubbleGrid[1]);
+    bubbleGrid.findThirdPoint(bubbleGrid.bubbleGrid[1], bubbleGrid.bubbleGrid[2]);
 
-var findThirdPoint = function(circle1, circle2) {
-
-  var newBub = {};
-
-  // case they are vertically aligned
-  if (circle1.x === circle2.x) {
-    //check for existing circle on one side
-    newBub.x = circle1.x - Math.sqrt(3) * radius;
-
-    bubbleGrid.forEach(function(bubble) {
-      if (bubble.x === circle1.x - Math.sqrt(3) * radius) {
-        newBub.x = circle1.x + Math.sqrt(3) * radius;
-      }
-    })
-
-    if (circle1.y > circle2.y) {
-      newBub.y = circle1.y - radius
-    } else {
-      newBub.y = circle2.y - radius;
-    }
-
-    console.log(newBub)
-
-    //push out new bubble
-    bubbleGrid.push(newBub)
-
-
-  } else {
-
-    if (circle1.x > circle2.x) {
-      if (circle1.y > circle2.y) {
-        newBub.y = circle1.y - diameter
-      } else {
-        newBub.y = circle1.y + diameter
-      }
-    }
-
-  }
-
-  //case they are not vertically aligned
-
-
-
-}
-
-var bubbleGrid = [];
-
-var bubbleGridPosition = function(radius) {
-
-  //establish circle sizes
-  var diameter = radius * 2;
-
-  //build the array
-
-
-  //build the starter circle
-  var C1YPosition = fullheight / 2;
-  var C1XPosition = fullwidth / 2;
-
-
-  bubbleGrid.push({
-    x: C1XPosition,
-    y: C1YPosition
+    bubbleGrid.renderBubbleGrid($('#work'), circleRadii * 2);
   });
-  bubbleGrid.push({
-    x: bubbleGrid[0].x,
-    y: bubbleGrid[0].y - diameter
-  });
-  bubbleGrid.push({
-    x: bubbleGrid[0].x - (Math.sqrt(3) * radius),
-    y: bubbleGrid[0].y - radius
-  });
-  bubbleGrid.push({
-    x: bubbleGrid[0].x - (Math.sqrt(3) * radius),
-    y: bubbleGrid[0].y - radius
-  });
-
-  //position bubbles
-  // var C2XPosition = (C1XPosition);
-  // var C2YPosition = (C1YPosition - diameter);
-
-  // var C3XPosition = (C1XPosition - (Math.sqrt(3) * radius));
-  // var C3YPosition = (C1YPosition - radius);
-
-  // var C4XPosition = (C3XPosition);
-  // var C4YPosition = (C3YPosition + diameter);
-
-  // var C5XPosition = (C3XPosition);
-  // var C5YPosition = (C3YPosition - diameter);
-
-  // var C6XPosition = (C1XPosition + (Math.sqrt(3) * radius));
-  // var C6YPosition = (C1YPosition - radius);
-
-  //create bubbles
-  createBubble($('#work'), 1, bubbleGrid[0].x, bubbleGrid[0].y, diameter);
-  createBubble($('#work'), 2, bubbleGrid[1].x, bubbleGrid[1].y, diameter);
-  createBubble($('#work'), 3, bubbleGrid[2].x, bubbleGrid[2].y, diameter);
-  // createBubble($('#work'), 4, C4XPosition, C4YPosition, diameter);
-  // createBubble($('#work'), 5, C5XPosition, C5YPosition, diameter);
-  // createBubble($('#work'), 6, C6XPosition, C6YPosition, diameter);
-
-
-};
-
-//  $(window).load(function() {
-//     // start up after 2sec no matter what
-//     window.setTimeout(function(){
-//         $('body').removeClass("loading").addClass('loaded');
-//     }, 500);
-//   bubbleGridPosition();
-
-// }); // end load
-
-// $(window).on("resize", function(){
-//   console.log('firing');
-//   bubbleGridPosition();
-
-// });
---
+++ js/grid.js
@@ -0,0 +1,159 @@
+function BubbleGrid(radius, numCircles) {
+
+  this.bubbleGrid = [];
+  this.radius = radius;
+  this.diameter = radius * 2;
+
+
+  this.bubbleGridStart = function(radius) {
+
+    this.bubbleGrid.push({
+      x: startPos.x,
+      y: startPos.y
+    });
+    this.bubbleGrid.push({
+      x: this.bubbleGrid[0].x,
+      y: this.bubbleGrid[0].y - this.diameter
+    });
+    this.bubbleGrid.push({
+      x: this.bubbleGrid[0].x - (Math.sqrt(3) * this.radius),
+      y: this.bubbleGrid[0].y - this.radius
+    });
+
+  };
+
+  this.findThirdPoint = function(one, two) {
+
+    var newBub = {};
+
+    if (one.x === two.x) {
+      //default
+      newBub.x = one.x + Math.sqrt(3) * this.radius;
+      //check for existing bubble on one side
+      this.bubbleGrid.forEach(function(bubble) {
+        if (bubble.x === newBub.x) {
+          newBub.x = one.x - Math.sqrt(3) * this.radius;
+        }
+      });
+
+      if (one.y > two.y) {
+        newBub.y = one.y - this.radius;
+      } else {
+        newBub.y = two.y - this.radius;
+      }
+
+      //case they are not vertically aligned
+    } else if (one.x > two.x) {
+
+      if (one.y > two.y) {
+
+        newBub.y = one.y - this.diameter;
+        newBub.x = one.x;
+
+        //check for existing bubble on one side
+        this.bubbleGrid.forEach(function(bubble) {
+          if (bubble.x === newBub.x) {
+            newBub.y = two.y + this.diameter;
+            newBub.x = two.x;
+          }
+        });
+
+      } else {
+        newBub.y = one.y + this.diameter;
+        newBub.x = one.x;
+        //check for existing bubble on one side
+        this.bubbleGrid.forEach(function(bubble) {
+          if (bubble.x === newBub.x) {
+            newBub.y = two.y - this.diameter;
+            newBub.x = two.x;
+          }
+        });
+      }
+    } else if (two.x > one.x) {
+
+      if (one.y > two.y) {
+
+        newBub.y = one.y - this.diameter;
+        newBub.x = one.x;
+
+        //check for existing bubble on one side
+        this.bubbleGrid.forEach(function(bubble) {
+          if (bubble.x === newBub.x) {
+            newBub.y = two.y + this.diameter;
+            newBub.x = two.x;
+          }
+        });
+
+      } else if (two.y > one.y) {
+        newBub.y = one.y + this.diameter;
+        newBub.x = one.x;
+        //check for existing bubble on one side
+        this.bubbleGrid.forEach(function(bubble) {
+          if (bubble.x === newBub.x) {
+            newBub.y = two.y - this.diameter;
+            newBub.x = two.x;
+          }
+        });
+      }
+    }
+
+    //push out new bubble
+    this.bubbleGrid.push(newBub);
+
+  };
+
+  this.renderBubbleGrid = function(div, diameter) {
+    console.log(this.bubbleGrid);
+    this.bubbleGrid.forEach(function(bubble, i) {
+      var html = '<div class="circle" id="C' + i + '"><div><h3>content</h3></div></div>';
+      div.append(html);
+      $('#C' + i).css('top', bubble.y);
+      $('#C' + i).css('left', bubble.x);
+      $('#C' + i).css('width', diameter).css('height', diameter);
+    });
+  };
+
+}
+
+
+
+// var bubbleGridPosition = function(radius) {
+
+//   //establish circle sizes
+//   var diameter = radius * 2;
+
+
+//   //build the starter circle
+//   var C1YPosition = fullheight / 2;
+//   var C1XPosition = fullwidth / 2;
+
+
+//   bubbleGrid.push({
+//     x: C1XPosition,
+//     y: C1YPosition
+//   });
+//   bubbleGrid.push({
+//     x: bubbleGrid[0].x,
+//     y: bubbleGrid[0].y - diameter
+//   });
+//   bubbleGrid.push({
+//     x: bubbleGrid[0].x - (Math.sqrt(3) * radius),
+//     y: bubbleGrid[0].y - radius
+//   });
+
+//   findThirdPoint(bubbleGrid[0], bubbleGrid[1], radius);
+//   findThirdPoint(bubbleGrid[1], bubbleGrid[2], radius);
+//   findThirdPoint(bubbleGrid[0], bubbleGrid[2], radius);
+//   findThirdPoint(bubbleGrid[5], bubbleGrid[0], radius);
+//   findThirdPoint(bubbleGrid[1], bubbleGrid[3], radius);
+
+
+//   createBubble($('#work'), 1, bubbleGrid[0].x, bubbleGrid[0].y, diameter);
+//   createBubble($('#work'), 2, bubbleGrid[1].x, bubbleGrid[1].y, diameter);
+//   createBubble($('#work'), 3, bubbleGrid[2].x, bubbleGrid[2].y, diameter);
+//   createBubble($('#work'), 4, bubbleGrid[3].x, bubbleGrid[3].y, diameter);
+//   createBubble($('#work'), 5, bubbleGrid[4].x, bubbleGrid[4].y, diameter);
+//   createBubble($('#work'), 6, bubbleGrid[5].x, bubbleGrid[5].y, diameter);
+
+
+// };
